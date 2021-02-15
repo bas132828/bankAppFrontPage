822520f29ad70207ffb08599d7bf1fce4dd900e5
Bas <bas1328@mail.ru>
2021-02-15 08:56:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,84 +32,39 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// console.log(document.documentElement);
-
-// const header = document.querySelector(".header");
-// const allSelection = document.querySelectorAll(".section");
-// // console.log(allSelection);
-
-// document.getElementById("section--1");
-// const allButtons = document.getElementsByTagName("button");
-// // console.log(allButtons);
-
-// document.getElementsByClassName("btn");
-
-//creating and inserting html
-
-// .insertAdjacentHTML
-
-// const message = document.createElement("div");
-// message.classList.add("cookie-message");
-// // message.textContent = 'We use cookies for smth';
-// message.innerHTML =
-//   'We use cookies for smth <button class = "btn btn--close--cookie">Got it!</button>';
-
-// header.append(message);
-// header.append(message);
-// header.append(message.cloneNode(true));
-// // console.log(message);
-// const moven = [1, 3, 5, 3, 2, 6, 23, 2, 6, 3, 4];
-
-// const temp = moven.reduce((acc, cur) => {
-//   if (acc > cur) return acc;
-//   else return cur;
-// }, moven[0]);
-
-// console.log(temp);
-
-// header.before(message);
-// header.after(message.cloneNode(true));
-// document
-//   .querySelector(".btn--close--cookie")
-//   .addEventListener("click", function () {
-//     message.remove();
-//   });
-
-// message.style.backgroundColor = "#37383d";
-
-// message.style.height =
-//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
-
-// document.documentElement.style.setProperty("--color-primary", "orangered");
-
-// // attributes
-
-// const logo = document.querySelector(".nav__logo");
-// console.log(logo.alt);
-// console.log(logo.src);
-
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
 btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: "smooth",
-  // });
   section1.scrollIntoView({ behavior: "smooth" });
 });
 
-//rgb(255,255,255);
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () =>
-  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+// Page navigation
 
-console.log(randomColor(0, 255));
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+//Add event listener to common parent element
+//Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  // console.log(e.target);
+  e.preventDefault();
+  //mathching
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// console.log(randomColor(0, 255));
